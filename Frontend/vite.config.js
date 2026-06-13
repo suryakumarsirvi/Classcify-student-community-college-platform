15c2d0950fcfeb99dc26def19c018ce120106fec
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-06-14 02:01:15 +0530

    v1

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://the-classcify-server.onrender.com',
         changeOrigin: true,
       },
     }
