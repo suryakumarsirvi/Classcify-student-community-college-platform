324a9b12c635994fd61e2d643143f1aa18ef7f84
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-06-15 11:05:03 +0530

    fix: Speed insights vercel remove

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "@/app/App.css";
 import App from "@/app/App.jsx";
-import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById("root")).render(
-  <SpeedInsights>
     <App />
-  </SpeedInsights>
 );
